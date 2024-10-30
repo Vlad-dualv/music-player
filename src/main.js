5e932654fc537eb588ac7b81867689e3750592dd
Vlad <vladswork95@gmail.com>
2024-10-30 11:00:32 +0000

    Main functions ready

--- src/main.js
+++ src/main.js
@@ -1,10 +1,53 @@
 const controls = {
   backwardBtn: document.querySelector('.backward-icon'),
   forwardBtn: document.querySelector('.forward-icon'),
-  playBtn: document.getElementById('play-icon'),
+  playControl: document.querySelector('.play-control'),
 };
 
 const progress = document.getElementById('progress');
 const song = document.getElementById('song');
+let isPlaying = false;
 
-song.onloadedmetadata = function () {};
+song.onloadedmetadata = function () {
+  progress.max = song.duration;
+  progress.value = song.currentTime;
+};
+
+controls.playControl.addEventListener('click', playPause);
+
+function playPause() {
+  if (isPlaying) {
+    song.pause();
+    controls.playControl.innerHTML = `<svg
+              class="icon play-icon"
+              width="40"
+              height="40"
+              aria-label="play icon"
+            >
+              <use href="./img/icons.svg#icon-play3"></use>
+            </svg>`;
+    isPlaying = false;
+  } else {
+    song.play();
+    controls.playControl.innerHTML = `<svg
+              class="icon play-icon"
+              width="40"
+              height="40"
+              aria-label="pause icon"
+            >
+              <use href="./img/icons.svg#icon-pause2"></use>
+            </svg>`;
+    isPlaying = true;
+  }
+}
+
+song.addEventListener('timeupdate', () => {
+  progress.value = song.currentTime;
+});
+
+progress.oninput = function () {
+  song.play();
+  song.currentTime = progress.value;
+  isPlaying = false;
+  playPause();
+};
