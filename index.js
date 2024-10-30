f47a8c66cc3ee7e6b97951b48a07006568fa53bd
Vlad-dualv <147710428+Vlad-dualv@users.noreply.github.com>
2024-10-30 12:32:26 +0000

    Deploying to gh-pages from @ Vlad-dualv/music-player@430acd9d0a223e49587a499beda9b7c9c0619480 🚀

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-(function(){const i=document.createElement("link").relList;if(i&&i.supports&&i.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))l(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const s of t.addedNodes)s.tagName==="LINK"&&s.rel==="modulepreload"&&l(s)}).observe(document,{childList:!0,subtree:!0});function u(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function l(e){if(e.ep)return;e.ep=!0;const t=u(e);fetch(e.href,t)}})();const c={backwardBtn:document.querySelector(".backward-icon"),forwardBtn:document.querySelector(".forward-icon"),playControl:document.querySelector(".play-control")},r=document.getElementById("progress"),o=document.getElementById("song");let n=!1;o.onloadedmetadata=function(){r.max=o.duration,r.value=o.currentTime};c.playControl.addEventListener("click",a);function a(){n?(o.pause(),c.playControl.innerHTML=`<svg
+(function(){const i=document.createElement("link").relList;if(i&&i.supports&&i.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))l(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const s of t.addedNodes)s.tagName==="LINK"&&s.rel==="modulepreload"&&l(s)}).observe(document,{childList:!0,subtree:!0});function u(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function l(e){if(e.ep)return;e.ep=!0;const t=u(e);fetch(e.href,t)}})();const c={backwardBtn:document.querySelector(".backward-icon"),forwardBtn:document.querySelector(".forward-icon"),playControl:document.querySelector(".play-control")},o=document.getElementById("progress"),r=document.getElementById("song");let n=!1;r.onloadedmetadata=function(){o.max=r.duration,o.value=r.currentTime};c.playControl.addEventListener("click",a);function a(){n?(r.pause(),c.playControl.innerHTML=`<svg
               class="icon play-icon"
               width="40"
               height="40"
               aria-label="play icon"
             >
-              <use href="./img/icons.svg#icon-play3"></use>
-            </svg>`,n=!1):(o.play(),c.playControl.innerHTML=`<svg
+              <use href=".src/img/icons.svg#icon-play3"></use>
+            </svg>`,n=!1):(r.play(),c.playControl.innerHTML=`<svg
               class="icon play-icon"
               width="40"
               height="40"
               aria-label="pause icon"
             >
-              <use href="./img/icons.svg#icon-pause2"></use>
-            </svg>`,n=!0)}o.addEventListener("timeupdate",function(){r.value=o.currentTime});r.oninput=function(){o.play(),o.currentTime=r.value,n=!1,a()};
+              <use href=".src/img/icons.svg#icon-pause2"></use>
+            </svg>`,n=!0)}r.addEventListener("timeupdate",function(){o.value=r.currentTime});o.oninput=function(){r.play(),r.currentTime=o.value,n=!1,a()};
 //# sourceMappingURL=index.js.map
